86242590bdcf82daa45d4b918f476e6b9a544d1f
Anderson Begossi <andersonbegossi@gmail.com>
2022-02-07 11:07:58 -0300

    chore: add jest coverage configs

--- jest.config.js
+++ jest.config.js
@@ -8,5 +8,13 @@ module.exports = {
   transformIgnorePatterns: [
     "node_modules/(?!((jest-)?react-native|@react-native(-community)?)|expo(nent)?|@expo(nent)?/.*|@expo-google-fonts/.*|react-navigation|@react-navigation/.*|@unimodules/.*|unimodules|sentry-expo|native-base|react-native-svg)"
   ],
-  setupFiles: ["./jestSetupFile.js"]
+  setupFiles: ["./jestSetupFile.js"],
+  collectCoverage: true,
+  collectCoverageFrom: [
+    "src/**/*.tsx",
+    "!src/**/*.spec.tsx"
+  ],
+  coverageReporters: [
+    "lcov"
+  ]
 }
